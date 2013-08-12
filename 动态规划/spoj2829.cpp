cb11e13b843188cb0711d724000344cd4a4e7a89
Carey Mo <daerduoCarey@163.com>
2013-08-12 12:01:43 +0800

    ppca2013 homewoerk backup

--- 动态规划/spoj2829.cpp
+++ 动态规划/spoj2829.cpp
@@ -5,10 +5,6 @@ const int MOD=1000000000;
 int test_num,n,m;
 int c[51]={0},d[16]={0};
 int f[2][32769]={0};
-void add(int &x,int y)
-{
-    x+=y;x%=MOD;
-}
 int main()
 {
     freopen("spoj2829.in","r",stdin);
@@ -19,7 +15,6 @@ int main()
     {
         int t;
         scanf("%d%d",&n,&m);
-        memset(f,0,sizeof(f));
         for(int i=1;i<=n;++i) scanf("%d",&c[i]);
         t=m&1;
         for(int i=0;i<d[m];++i)
@@ -34,7 +29,7 @@ int main()
                 {
                     if(j&d[m-1])
                     {
-                        add(f[t][j&(d[m-1]-1)],f[t^1][j]);
+                        f[t][j&(d[m-1]-1)]=(f[t][j&(d[m-1]-1)]+f[t^1][j])%MOD;
                     }
                     else
                     {
@@ -46,7 +41,6 @@ int main()
             for(int j=1;j<m;++j)
             {
                 t=t^1;
-                memset(f[t],0,sizeof(f[t]));
                 for(int k=0;k<d[m];++k)
                 {
                     if((k&d[m-j-1])==0)
@@ -56,7 +50,7 @@ int main()
                     }
                     else
                     {
-                        add(f[t][k&(~d[m-j-1])],f[t^1][k]);
+                        f[t][k&(~d[m-j-1])]=(f[t][k&(~d[m-j-1])]+f[t^1][k])%MOD;
                     }
                 }
             }
